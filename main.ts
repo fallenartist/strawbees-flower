f6e8a81aafe88db13097f900182e6e3198b5c4f0
Aleksander Lenart <aleksander@lenart.pl>
2022-01-02 18:21:22 +0100

    Fine tune servo

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let light_level = 0
 let max_light = 15
 basic.forever(function () {
     light_level = input.lightLevel()
-    sb.setServoPosition(sb.servo(SBServo.ServoA), Math.map(light_level, 0, max_light, 0, 50))
+    sb.setServoPosition(sb.servo(SBServo.ServoA), Math.map(light_level, 0, max_light, 0, 3))
     if (light_level > max_light / 2) {
         basic.showLeds(`
             # . # . #
