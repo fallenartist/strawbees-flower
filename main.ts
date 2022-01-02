897d9b76c1caade63c4351fb13a165cbb303135c
Aleksander Lenart <aleksander@lenart.pl>
2022-01-02 17:29:33 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+let light_level = 0
+let max_light = 15
+basic.forever(function () {
+    light_level = input.lightLevel()
+    sb.setServoPosition(sb.servo(SBServo.ServoA), Math.map(light_level, 0, max_light, 0, 50))
+    if (light_level > max_light / 2) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
