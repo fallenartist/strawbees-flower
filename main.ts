e6fb5057a16940b2c7b4f5b486d767aeab2dbab2
Aleksander Lenart <aleksander@lenart.pl>
2022-01-03 11:07:59 +0100

    Adjusting numbers

--- main.ts
+++ main.ts
@@ -1,23 +1,7 @@
 let light_level = 0
-let max_light = 10
+let max_light = 25
 basic.forever(function () {
     light_level = input.lightLevel()
-    sb.setServoPosition(sb.servo(SBServo.ServoA), Math.map(light_level, 0, max_light, 0, 3))
-    if (light_level > max_light / 2) {
-        basic.showLeds(`
-            # . # . #
-            . # # # .
-            # # # # #
-            . # # # .
-            # . # . #
-            `)
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . # . .
-            . # . # .
-            . . . . .
-            `)
-    }
+    basic.showNumber(light_level)
+    sb.setServoPosition(sb.servo(SBServo.ServoA), Math.map(light_level, 0, max_light, 0, 5))
 })
