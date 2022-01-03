1565cc977fbac0ec7df8eab97eecee1e1b3f3b2b
Aleksander Lenart <aleksander@lenart.pl>
2022-01-03 09:01:04 +0100

    Adjusted light level

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 let light_level = 0
-let max_light = 15
+let max_light = 10
 basic.forever(function () {
     light_level = input.lightLevel()
     sb.setServoPosition(sb.servo(SBServo.ServoA), Math.map(light_level, 0, max_light, 0, 3))
